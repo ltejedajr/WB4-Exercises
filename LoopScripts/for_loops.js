445534618433a93aa0ed8f21850cae2475cad4fb
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-31 09:01:38 -0500

    Added practice labs to LoopScripts

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// for loop
+
+for (let i = 0; i < 7; i++) {
+    console.log("I love loops");
+}
+
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// while loop
+let i = 0
+
+while (i < 7) {
+    i++;
+    console.log("I love loops");
+}
